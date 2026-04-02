f6f0194b74f8e7c917611a893755cac9bd262810
Medwin <garmed172@gmail.com>
2026-04-02 14:21:00 +0800

    Add: Navbar

--- app/components/LandingPage/Footer.vue
+++ app/components/LandingPage/Footer.vue
@@ -1,3 +1,14 @@
 <template>
-  <div class="bg-primary text-center text-2xl font-bold">This is a Footer</div>
-</template>
+  <div class="bg-secondary">
+    <div class="flex items-center">
+      <h1 class="ml-12 pt-15 text-card text-2xl font-semibold">DermAssist – AI-Powered Skin Detection & Doctor Consultation</h1>
+      <div class="h-[5px] rounded-full mt-7 bg-card w-188"></div>
+    </div>
+    <div class="m-12 space-y--2">
+      <div class="font-barlow">
+        <p class="text-primary-light">Contact DermAssist at:</p>
+        <p class="text-card font-semibold text-2xl">dermassist@gmail.com</p>
+      </div>
+    </div>
+  </div>
+</template> 
